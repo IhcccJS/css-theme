914d8891781cc73a2a1267544404ba40b6bb49d9
liucdev <1058807007@qq.com>
2023-12-19 17:45:44 +0800

    feat: 完善功能

--- bin/build.js
+++ bin/build.js
@@ -36,54 +36,62 @@ const buildOptions = [
   },
 ];
 
-async function build(option) {
-  const indexExports = [];
-  const readPath = path.join(__dirname, THEME_PATH);
-  const writePath = path.join(__dirname, "../", option.output);
-  const files = fs.readdirSync(readPath);
+function createBuilder() {
+  const cache = {};
+  return async function build(option) {
+    const indexExports = [];
+    const readPath = path.join(__dirname, THEME_PATH);
+    const writePath = path.join(__dirname, "../", option.output);
+    const files = fs.readdirSync(readPath);
 
-  await forEachPromised(files, async (filePath) => {
-    const variables = await import(THEME_PATH + "/" + filePath).then(
-      (module) => module.default
-    );
-    const themeFileName = path.basename(filePath, ".js");
-    const themeName = camelCase(themeFileName);
-    const content = option.build(themeName, variables);
-    if (!fs.existsSync(writePath)) fs.mkdirSync(writePath);
-    fs.writeFileSync(
-      path.join(writePath, `${themeFileName}.${option.extname}`),
-      content,
-      { encoding: "utf-8" }
-    );
-    if (option.indexExport) {
-      if (option.output === "es") {
-        indexExports.push(
-          `export { default as ${themeName} } from "./${themeFileName}.${option.extname}";`
-        );
-      } else if (option.output === "lib") {
-        indexExports.push(
-          `exports.${themeName} = require("./${themeFileName}.${option.extname}");`
-        );
+    await forEachPromised(files, async (filePath) => {
+      const themeFileName = path.basename(filePath, ".json");
+      const themeName = camelCase(themeFileName);
+      const themeFilePath = path.join(__dirname, THEME_PATH, filePath);
+      let variables = cache[themeName];
+      if (!variables) {
+        variables = JSON.parse(fs.readFileSync(themeFilePath));
+        cache[themeName] = variables;
       }
+      const content = option.build(themeName, variables);
+      if (!fs.existsSync(writePath)) fs.mkdirSync(writePath);
+      fs.writeFileSync(
+        path.join(writePath, `${themeFileName}.${option.extname}`),
+        content,
+        { encoding: "utf-8" }
+      );
+      if (option.indexExport) {
+        if (option.output === "es") {
+          indexExports.push(
+            `export { default as ${themeName} } from "./${themeFileName}.${option.extname}";`
+          );
+        } else if (option.output === "lib") {
+          indexExports.push(
+            `exports.${themeName} = require("./${themeFileName}.${option.extname}");`
+          );
+        }
+      }
+    });
+
+    if (indexExports.length > 0) {
+      fs.writeFileSync(
+        path.join(writePath, `index.js`),
+        indexExports.join("\n"),
+        {
+          encoding: "utf-8",
+        }
+      );
     }
-  });
-
-  if (indexExports.length > 0) {
-    fs.writeFileSync(
-      path.join(writePath, `index.js`),
-      indexExports.join("\n"),
-      {
-        encoding: "utf-8",
-      }
-    );
-  }
+  };
 }
 
 async function main() {
   try {
+    let builder = createBuilder();
     await forEachPromised(buildOptions, async (option) => {
-      await build(option);
+      await builder(option);
     });
+    builder = null;
 
     fs.copyFileSync(
       path.join(__dirname, "../src/index.d.ts"),
--- src/ui/editor/index.jsx
+++ src/ui/editor/index.jsx
@@ -1,20 +1,45 @@
-// import { useState } from 'react';
+import React from "react";
+import camelCase from "lodash/camelCase";
+import Card from "./components/card";
 import "./index.css";
 
+function format(dataString) {
+  const items = dataString
+    .replace(/(\s|\n)+/g, "")
+    .split(";")
+    .filter((item) => !!item);
+  const json = items.reduce((data, item) => {
+    const [key, value] = item.split(":");
+    if (key && value) data[camelCase(key)] = value;
+    return data;
+  }, {});
+  return JSON.stringify(json, null, 2);
+}
+
 function App() {
+  const [input, setInput] = React.useState("");
+
+  const output = React.useMemo(() => {
+    if (!input) return "";
+    return format(input);
+  }, [input]);
+
   return (
-    <>
+    <div className="page">
       <h1>CSS Theme Editor</h1>
-      <div className="card">
-        <h3>Color</h3>
-      </div>
-      <div className="card">
-        <h3>Size</h3>
-      </div>
-      <div className="card">
-        <h3>Style</h3>
-      </div>
-    </>
+      <Card>
+        <h3>CSS 变量：</h3>
+        <textarea
+          rows="10"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </Card>
+      <Card>
+        <h3>Output：</h3>
+        <textarea rows="20" value={output} readOnly />
+      </Card>
+    </div>
   );
 }
 
